0aefc60aa9d437cfbf5587dd3cdaa5d05dbf4fed
Murillo <murillo.ventura1711@unifei.edu.br>
2020-08-28 14:07:06 -0300

    initial commit

---
+++ staticLinked.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+
+using namespace std;
+
+#define TAM 10
+struct No
+{
+    char Info;
+    int Lig;
+};
+
+struct ListaEstaticaEncadeada
+{
+    int Com;
+    int Dispo;
+    No Dados[TAM];
+};
+
+void IniciaLista (ListaEstaticaEncadeada& L)
+{
+    for (int i=0; i<TAM; i++)
+        L.Dados[i].Lig = i+1;
+    L.Dados[TAM-1].Lig = -1;
+    L.Dispo = 0;
+    L.Com = -1;
+}
+bool InsereLista (ListaEstaticaEncadeada& L, char Novo)
+{
+    if (L.Dispo == -1)return false;
+    else
+    {
+        if (L.Com == -1)
+        {
+            L.Com = L.Dispo;
+            L.Dispo = L.Dados[L.Dispo].Lig;
+            L.Dados[L.Com].Info = Novo;
+            L.Dados[L.Com].Lig = -1;
+        }
+        else
+        {
+            int Ant = -1;
+            int Aux = L.Com;
+            while ((Aux != -1) && (L.Dados[Aux].Info < Novo))
+            {
+                Ant = Aux;
+                Aux = L.Dados[Aux].Lig;
+            }
+            Aux = L.Dispo;
+            L.Dispo = L.Dados[L.Dispo].Lig;
+            L.Dados[Aux].Info = Novo;
+            if (Ant == -1)
+            {
+                L.Dados[Aux].Lig = L.Com;
+                L.Com = Aux;
+            }
+            else
+            {
+                L.Dados[Aux].Lig = L.Dados[Ant].Lig;
+                L.Dados[Ant].Lig = Aux;
+            }
+        }
+        return true;
+    }
+}
+
+bool RetiraLista (ListaEstaticaEncadeada& L, char Novo)
+{
+    int Ant = -1;
+    int Aux = L.Com;
+    while ((Aux != -1) && (L.Dados[Aux].Info != Novo))
+    {
+        Ant = Aux;
+        Aux = L.Dados[Aux].Lig;
+    }
+    if (Aux == -1)return false;
+    if (Aux == L.Com)L.Com = L.Dados[L.Com].Lig;
+    else L.Dados[Ant].Lig = L.Dados[Aux].Lig;
+    L.Dados[Aux].Lig = L.Dispo;
+    L.Dispo = Aux;
+    return true;
+}
+
+void mostraLista(ListaEstaticaEncadeada L){
+    int prox = L.Com;
+    while(prox!=-1)
+    {
+        printf("\nL(%i) = %c ; -> %i",prox, L.Dados[prox].Info, L.Dados[prox].Lig);
+        prox=L.Dados[prox].Lig;
+    }
+}
+
+int main(){
+    ListaEstaticaEncadeada L;
+    IniciaLista(L);
+    InsereLista(L,'F');InsereLista(L,'A');InsereLista(L,'B');
+    mostraLista(L);
+    return 0;
+}
---
+++ staticSeq.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+
+using namespace std;
+
+#define TAM 10
+struct No
+{
+    char Chave;
+    int Valor;
+};
+struct ListaSequencial
+{
+    No Dados[TAM];
+    int Fim;
+};
+
+bool InsereListaSeq (ListaSequencial &L, No Novo)
+{
+    if (L.Fim == TAM - 1)
+        return false;
+    int i = 0, p;
+    while ((i <= L.Fim) && (L.Dados[i].Chave < Novo.Chave))
+        i++;
+    p = i;
+    i = L.Fim;
+    while (i > p-1)
+    {
+        L.Dados[i+1] = L.Dados[i];
+        i--;
+    }
+    L.Dados[p] = Novo;
+    L.Fim++;
+    return true;
+}
+
+bool RetiraListaSeq (ListaSequencial& L, No& N)
+{
+    int i = 0;
+    while ((i <= L.Fim) && (L.Dados[i].Chave != N.Chave))
+        i++;
+    if (i > L.Fim)
+        return false;
+    N = L.Dados[i];
+    while (i < L.Fim)
+    {
+        L.Dados[i] = L.Dados[i+1];
+        i++;
+    }
+    L.Fim--;
+    return true;
+}
+void iniciaLista(ListaSequencial& L)
+{
+    L.Fim=-1;
+}
+bool buscaBin(ListaSequencial L, char chave)
+{
+    if(L.Fim<0)
+        return false;
+    int inicio=0, fim=L.Fim, m;
+    while(true){
+        m = (inicio+fim)/2;
+        if(inicio==fim && L.Dados[m].Chave!=chave)
+            return false;
+        if(chave>L.Dados[m].Chave)
+            inicio=m+1;
+        else if(chave<L.Dados[m].Chave)
+            fim = m;
+        else
+            return true;
+    }
+}
+
+
+void mostraLista(ListaSequencial L)
+{
+    for(int i=0;i<=L.Fim;i++)
+        printf("\nL(%i) = %i ; %c", i, L.Dados[i].Valor, L.Dados[i].Chave);
+}
+int main()
+{
+    ListaSequencial L;
+    No no1, no2, no3;
+    no1.Chave='F';no1.Valor=1;
+    no2.Chave='A';no2.Valor=2;
+    no3.Chave='B';no3.Valor=3;
+    iniciaLista(L);
+    InsereListaSeq(L,no1);
+    InsereListaSeq(L,no2);
+    InsereListaSeq(L,no3);
+    mostraLista(L);
+    if(buscaBin(L,'F'))
+        printf("\nAchei F!!");
+    else
+        printf("\nNao achei F");
+    return 0;
+}
